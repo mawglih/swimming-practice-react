375eb5b9851ca74d60fceb2dae1e7ec813f82bf1
Oleg Markoff <mawglih@gmail.com>
2018-03-15 16:21:06 -0400

    header2

--- src/components/header.js
+++ src/components/header.js
@@ -4,26 +4,24 @@ import { NavLink } from 'react-router-dom';
 class Header extends Component {
     render() {
         return(
-            <div>
+            <div className="header">
                 <div className="header__logo">Logo</div>
                 <div className="header__nav">
-                    <ul className="header__nav-ul">
-                        <li className="header__nav-item">
-                            <NavLink activeClassName="selected__link" to="/main">Home</NavLink>
-                        </li>
-                        <li className="header__nav-item">
-                            <NavLink activeClassName="selected__link" to="/login">Login</NavLink>
-                        </li>
-                        <li className="header__nav-item">
-                            <NavLink activeClassName="selected__link" to="/signup">SignUp</NavLink>
-                        </li>
-                        <li className="header__nav-item">
-                            <NavLink activeClassName="selected__link" to="/logout">Logout</NavLink>
-                        </li>
-                        <li className="header__nav-item">
-                            <NavLink activeClassName="selected__link" to="/training">Training</NavLink>
-                        </li>
-                    </ul>
+                    <div className="header__nav-item">
+                        <NavLink activeClassName="selected__link" to="/main">Home</NavLink>
+                    </div>
+                    <div className="header__nav-item">
+                        <NavLink activeClassName="selected__link" to="/login">Login</NavLink>
+                    </div>
+                    <div className="header__nav-item">
+                        <NavLink activeClassName="selected__link" to="/signup">SignUp</NavLink>
+                    </div>
+                    <div className="header__nav-item">
+                        <NavLink activeClassName="selected__link" to="/logout">Logout</NavLink>
+                    </div>
+                    <div className="header__nav-item">
+                        <NavLink activeClassName="selected__link" to="/training">Training</NavLink>
+                    </div>
                 </div>
             </div>
         );
